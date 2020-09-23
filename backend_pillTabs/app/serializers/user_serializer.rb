eb1e14b06d67fb0a2217e16d77f446e5fb379442
Raza <rtshareef@gmail.com>
2020-09-23 16:24:38 -0400

    wip

--- backend_pillTabs/app/serializers/user_serializer.rb
+++ backend_pillTabs/app/serializers/user_serializer.rb
@@ -1,3 +1,3 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id,:first_name,:category
+  attributes :id,:first_name,:answer1,:answer2,:answer3,:answer4,:answer5,:answer6,:answer7,:answer8,:category
 end
